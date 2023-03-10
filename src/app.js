cef3260faeea2714a53ec665d1203d00b3fe00dc
Deborah Dobles <deb.dob.m@gmail.com>
2023-03-10 07:26:39 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    let randomNumber = Math.random() * 10;
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["sprinter", "racoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "rob my"];
+  let possession = ["homework", "handbag", "car", "shoe", "workbook"];
+  let where = [
+    "on the street",
+    "in my house",
+    "in my driveway",
+    "in the parking lot"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex] +
+    " "
+  );
 };
